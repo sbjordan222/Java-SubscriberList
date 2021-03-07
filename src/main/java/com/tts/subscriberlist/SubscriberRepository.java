f790bb99a0d351320b6a9972f9fdd81c0a40f9f8
Jordan <71449357+sbjordan222@users.noreply.github.com>
2021-03-06 21:50:06 -0600

    first commit

---
+++ src/main/java/com/tts/subscriberlist/SubscriberRepository.java
@@ -0,0 +1,8 @@
+package com.tts.subscriberlist;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+	
+}
+
